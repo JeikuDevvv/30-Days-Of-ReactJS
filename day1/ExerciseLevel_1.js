b95bf78906874b3e67a9d9f2170281a82bb9609f
Jeiku Dev <shippai.jeikudev@gmail.com>
2022-11-24 16:55:03 +0800

    day/1 1-6 Exercise

--- day1/ExerciseLevel_1.js
+++ day1/ExerciseLevel_1.js
@@ -1,6 +1,6 @@
-// Declare an empty array
+// (1) Declare an empty array
 const cars = [];
-// Declare an array with more than 5 number of elements
+// (2) Declare an array with more than 5 number of elements
 const numbers = [
   'one',
   'two',
@@ -13,14 +13,14 @@ const numbers = [
   'nine',
 ];
 
-// Find the length of your array
+// (3) Find the length of your array
 const carLength = cars.length;
 console.log('Array Cars Length: ' + carLength);
 
 const numberLength = numbers.length;
 console.log('Array Numbers Length: ' + numberLength);
 
-// Get the first item, the middle item and the last item of the array
+// (4) Get the first item, the middle item and the last item of the array
 const first = numbers[0];
 const middle = numbers[Math.floor(numberLength / 2)];
 const last = numbers[carLength - 1];
@@ -29,10 +29,10 @@ console.log('First Item: ' + first);
 console.log('Middle Item: ' + middle);
 console.log('Last Item: ' + last);
 
-// Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+// (5) Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
 const mixedDataTypes = [cars, numbers, 'fruits', 'dates', 'days', 'books'];
 
 const mixedLength = mixedDataTypes.length;
 console.log('Array mixedDataTypes Length: ' + mixedLength);
 
-// Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+// (6) Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
